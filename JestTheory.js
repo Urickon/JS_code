32a6960e698dca8592d64b73a0096e9527fd1504
Urickon <Rodoslavak@yandex.ru>
2021-04-21 15:01:22 +0300

    .

---
+++ JestTheory.js
@@ -0,0 +1,21 @@
+JEST
+
+//===================================Unit-testing
+/*In other words module testing, block testing. It's about isolation and separation of code blocks, some small parts of the program.
+Tests should be written for every significant function or method.*/
+
+
+//===================================TDD  - test driving development
+/*Its a way to develop software, based on short developing cycles. First developer create test, and then write code 
+for this test. In a nutshell you can think about it as a conception "Tests first, code after"*/
+
+
+//===================================Common principle of working
+
+
+
+
+
+
+
+
