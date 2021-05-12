a580884282fa24ddafe4b8b8a92ee9ca5adac77c
Urickon <Rodoslavak@yandex.ru>
2021-05-12 15:59:01 +0300

    .

--- React_theory.js
+++ React_theory.js
@@ -2,17 +2,21 @@
 
 =====================================================================================================
 
-REACT CONCEPTS (hard theory)
-
 HOW TO LEARN
 
 /*
 1. Always draw schemas, recap and outline your knowledges after every lesson.
 2. Focus only on you study subject. In this case it is React, dont do a lot of markup or styling.
-3. Try to understand how it works, not only how to write code on react.
+3. Try to understand how it works, not only how to write code on React.
+4. Highlite on new features and focus on them but understand old features (like Class components in React)
+5. Think about problems, which React is solving
 */
 
-======================================================================================================
+=====================================================================================================
+
+REACT CONCEPTS (hard theory)
+Virtual DOM
+/**/
 
 JSX
 /*Format combining data and markup.*/
