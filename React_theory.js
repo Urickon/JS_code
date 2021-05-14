ee50cf9f7f846c6ec6267cb225c475b97e90fbf4
Urickon <Rodoslavak@yandex.ru>
2021-05-14 12:49:48 +0300

    .

--- React_theory.js
+++ React_theory.js
@@ -6,7 +6,7 @@ HOW TO LEARN
 
 /*
 1. Always draw schemas, recap and outline your knowledges after every lesson.
-2. Focus only on you study subject. In this case it is React, dont do a lot of markup or styling.
+2. Focus only on your study subject. In this case it is React, dont do a lot of markup or styling.
 3. Try to understand how it works, not only how to write code on React.
 4. Highlite on new features and focus on them but understand old features (like Class components in React)
 5. Think about problems, which React is solving
@@ -15,8 +15,29 @@ HOW TO LEARN
 =====================================================================================================
 
 REACT CONCEPTS (hard theory)
+
+React Create App
+/*
+It is a set of tools which helps to build React App with one click. It includes babel and webpack.
+It has one main dependency - react-scripts to manage the hole App.
+react-scripts - is a container for babel and webpack.
+npm run eject - separates babel and webpack configs.
+
+You can successfully create a React application without using React Create App, and for advanced development it may be better.. 
+*/
+
 Virtual DOM
-/**/
+/*
+Evelution of web is decreasing markup and increasing JavaScript. 
+
+In very old days browsers reloaded page every time when some new info from server was received.
+Today server just sens a JSON, and JS on the client side puts it into markup without reloading page.
+We can simply change one part of the App and keep another parts safe.
+
+Today we have a lot of JS and radically small initial HTML in our page. We have lots of logic in our site, so it is better to call it web Application.
+Site  - is an old 
+React maximizes this concept.
+*/
 
 JSX
 /*Format combining data and markup.*/
